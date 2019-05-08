ab87a3e227709230ec51789066bab1069262d197
GameNeverOver <1027997726@qq.com>
2019-05-08 10:02:56 +0800

    init

---
+++ src/store.js
@@ -0,0 +1,10 @@
+// import {observable,action} from "mobx";
+// class Store {
+//     id = Math.random();
+//     @observable title="飞飞";
+//     @observable finished = false;
+//     @action cTitle =()=>{
+//         this.title="麦扣扣"
+//     }
+// }
+// export  default  new Store()
